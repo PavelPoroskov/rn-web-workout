16232506b912dbf2c051fc92d180a6c15e06ff64
PavelPoroskov <pavel.poroskov.se@gmail.com>
2026-03-22 21:09:59 +0300

    add decorators

--- packages/common/src/stores/CounterStore.ts
+++ packages/common/src/stores/CounterStore.ts
@@ -1,30 +1,31 @@
-import { makeObservable, observable, action } from "mobx";
+// import { makeObservable, observable, action } from "mobx";
+import { observable, action } from "mobx";
 import { createContext } from "react";
 
-class CounterStore {
-    count = 0
-
-    constructor() {
-        makeObservable(this, {
-            count: observable,
-            incrementCount: action.bound
-        })
-    }
-
-    incrementCount() {
-        this.count += 1
-    }
-}
-
 // class CounterStore {
-//     @observable
 //     count = 0
 
-//     @action.bound
+//     constructor() {
+//         makeObservable(this, {
+//             count: observable,
+//             incrementCount: action.bound
+//         })
+//     }
+
 //     incrementCount() {
 //         this.count += 1
 //     }
 // }
+
+class CounterStore {
+    @observable
+    accessor count = 0
+
+    @action.bound
+    incrementCount() {
+        this.count += 1
+    }
+}
 
 export const CounterStoreContext = createContext<CounterStore>(new CounterStore())
 // export const counterStore = new CounterStore()
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite'
 import { rnw } from "vite-plugin-rnw";
-// import type { Options } from '@vitejs/plugin-react'
+import type { Options } from '@vitejs/plugin-react'
 
-// const options: Options = {
-//   babel: {
-//     parserOpts: {
-//       plugins: [
-//         'decorators',
-//       ]
-//     }
-//   }
-// }
+const options: Options = {
+  babel: {
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { "version": "2023-11" }],
+    ]
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [rnw()],
-  // plugins: [rnw(options)],
+  // plugins: [rnw()],
+  plugins: [rnw(options)],
 })
